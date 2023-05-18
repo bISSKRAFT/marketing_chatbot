7da164523224fc530c455edea1d1bf0345f78824
bISSKR4FT <donisch19473@hs-ansbach.de>
2023-05-18 09:32:59 +0000

    refactor: delted db because of crawler

--- actions/actions.py
+++ actions/actions.py
@@ -33,6 +33,15 @@ from rasa_sdk import Action, Tracker
 
 # source: https://learning.rasa.com/conversational-ai-with-rasa/custom-actions/
 
+opening_times_db = {
+    'april-bis-oktober': 'Mo. - So.: 10:00 bis 18:00 Uhr',
+    'november': 'Mo. - So.: 13:00 bis 16:00 Uhr',
+    'dezember-bis-januar': 'bis 8. Januar: Mo. - So.: 11:00 bis 17:00 Uhr',
+    'januar-bis-maerz': 'ab 9. Januar: Mo. - So.: 13:00 bis 16:00 Uhr',
+    'weihnachten': 'An Heiligabend haben wir für Sie von 10:00 bis 13:00 Uhr geöffnet',
+    'silvester': 'An Silvester haben wir für Sie von 10:00 bis 13:00 Uhr geöffnet',
+}
+
 class ActionGetOpeningTimes(Action):
 
     def name(self) -> Text:
@@ -44,12 +53,12 @@ class ActionGetOpeningTimes(Action):
         html = requests.get(url).text
         soup = BeautifulSoup(html, "html.parser")
         soup.find_all("div", class_="wpb_wrapper")[1].text.strip()
-        
+
         #TODO: extract right opening times from soup with tracker information
         current_entity = next(tracker.get_latest_entity_values("holiday"), None)
 
         if not current_entity:
             # TODO: was wenn kein "holiday" erkannt wurde?
-            msg = "Wenn du etwas über die Öffnungszeiten wissen möchtest, dann frage mich einfach nach Öffnungszeiten"
+            msg = "Es tut mir leid, das habe ich nicht richtig verstanden. Wenn du etwas über die Öffnungszeiten wissen möchtest, dann frage einfach nach Öffnungszeiten"
             dispather.utter_message(text=msg)
         pass
